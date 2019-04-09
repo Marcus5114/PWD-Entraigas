d904fa58571c542460aafd9649737e33707cf2d9
Fidel Entraigas <fentraigas5114@gmail.com>
2019-04-09 00:42:37 +0000

    TG-1 prueba

--- TP 1/TP1.php
+++ TP 1/TP1.php
@@ -6,7 +6,7 @@
 // cambio del profesor
 	include 'Cabecera.php';
 	include 'Izq.php';
-	require 'Centro.php';
+
 	include 'Der.php';
 ?>
 
@@ -25,6 +25,32 @@
 
 <body>
 	
+	<div class="centro">
+	
+		<form onsubmit="validacion()">
+			<label>Nombre:</label><input type="text" name="nombre"> <br>
+
+			<label>Edad:</label><input type="text" name="edad"> <br>
+
+			<label>Sexo:</label> <br>
+
+			M<input type="radio" name="sexo" value="1"> <br>
+
+			F<input type="radio" name="sexo" value="2"><br>
+
+			<label>Estudios:</label> <select name="estudios">
+				<option>Primario</option>
+				<option selected>Secundario</option>
+				<option>Terciario</option>
+				<option>Universitario</option>
+			</select>
+
+			<br>
+
+			<input type="submit" name="enviar" value="Enviar" onclick="validacion()">
+
+		</form>
+	</div>
 	
 	
 	
